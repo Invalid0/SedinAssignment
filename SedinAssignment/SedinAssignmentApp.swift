f2520550d02edf55ea7115af20fdbb0576cae4b0
Invalid0 <darshankmm2@gmail.com>
2023-11-01 14:46:13 +0530

    Initial Commit

---
+++ SedinAssignment/SedinAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SedinAssignmentApp.swift
+//  SedinAssignment
+//
+//  Created by EMPULSE on 01/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SedinAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
